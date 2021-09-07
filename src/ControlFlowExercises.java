df17a0f1125f3354219629cd6b470e0d749aec71
Paris Shirley <shirleyp1097@gmail.com>
2021-09-07 13:48:57 -0500

    begin string exercises

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.printf("%s ", i);
+//            i++;
+//        }
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i < 1000000);
+//        for (long i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
+//        for (long i = 2; i < 1000000; i *= i) {
+//            System.out.println(i);
+//        }
+//        for (long i = 1; i <= 100; i++) {
+//            if ((i % 3 == 0) && (i % 5 == 0)) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+//        Scanner myScanner = new Scanner(System.in);
+//        System.out.println("What number would you like to go up to? ");
+//        long userInt = myScanner.nextInt();
+//        long starter = 0;
+//        System.out.println("Here is your table!");
+//        System.out.print("Number: ");
+//        while (starter <= userInt) {
+//            System.out.printf("%s ", starter);
+//            System.out.println(starter * starter);
+//            starter++;
+//        }
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Input a grade: ");
+//        long grade = myScanner.nextInt();
+//        if (grade <= 59) {
+//            System.out.println("F");
+//        } else {
+//            System.out.println("A");
+//        }
+    }
+}

de4082e6b642bbd449d96abbc4a196c8842d18fa
Paris Shirley <shirleyp1097@gmail.com>
2021-09-15 09:02:44 -0500

    begin abstract/interface exercises

---
+++ src/shapes/Quadrilateral.java
@@ -0,0 +1,27 @@
+package shapes;
+
+public abstract class Quadrilateral extends Shape implements Measurable {
+    protected double length;
+    protected double width;
+
+    Quadrilateral(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public abstract setLength() {
+
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,18 +1,18 @@
 package shapes;
 
 public class Square extends Rectangle{
-    private double side;
-
-    public Square(double side) {
-        super(side, side);
-        this.side = side;
-    }
-
-    public double getArea() {
-        return side * side;
-    }
-
-    public double getPerimeter() {
-        return side * 4;
-    }
+//    private double side;
+//
+//    public Square(double side) {
+//        super(side, side);
+//        this.side = side;
+//    }
+//
+//    public double getArea() {
+//        return side * side;
+//    }
+//
+//    public double getPerimeter() {
+//        return side * 4;
+//    }
 }

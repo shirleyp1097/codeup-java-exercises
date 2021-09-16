fd132009f77717716f629543b08a7d6538408f22
Paris Shirley <shirleyp1097@gmail.com>
2021-09-16 10:11:08 -0500

    begin collections exercise

---
+++ src/grades/Student.java
@@ -0,0 +1,35 @@
+package grades;
+
+import java.util.*;
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<Integer>();
+    }
+
+//    // returns the student's name
+    public String getName() {
+        return this.name;
+    }
+
+    public void addGrade(int grade) {
+        grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        int sum = 0;
+        for (int value: grades) {
+            sum += value;
+        }
+        return  sum / grades.size();
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}

74310a0f1364b26656bb007d08a4372d899e80e9
Paris Shirley <shirleyp1097@gmail.com>
2021-10-14 09:14:41 -0500

    complete warmup on thursday oct 14

---
+++ src/JavaDrills.java
@@ -0,0 +1,11 @@
+public class JavaDrills {
+
+    public static String flipOuterCase(String input) {
+        String output = "a";
+        return output;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(flipOuterCase("yo"));
+    }
+}

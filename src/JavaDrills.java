3d92333fc9e5e5dc5f3a76587e649aa5d21a7cac
Paris Shirley <shirleyp1097@gmail.com>
2021-10-15 09:32:30 -0500

    complete warmup friday

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -1,4 +1,9 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class JavaDrills {
+
 
     public static String flipOuterCase(String input) {
         String output = "";
@@ -16,11 +21,35 @@ public class JavaDrills {
 
     }
 
+    public static int returnTotalDifference(ArrayList<Integer> arr, ArrayList<Integer> arr2) {
+        int total = 0;
+        for (int i = 0; i < arr.size();  i++) {
+            total += arr.get(i);
+        }
+        for (int i = 0; i < arr2.size();  i++) {
+            total += arr2.get(i);
+        }
+        return total;
+    }
+
+    public static Dimension returnTotalDifference(List<Integer> arr, List arr2) {
+        int total = 0;
+        return arr.size();
+//        for (int i = 0; i < arr.size();  i++) {
+//            total += arr.get(i);
+//        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(flipOuterCase("you"));
-        System.out.println(flipOuterCase("cat")); // CaT
-        System.out.println(flipOuterCase("CaT")); // cat
-        System.out.println(flipOuterCase("caT")); // Cat
-        System.out.println(flipOuterCase("cAt")); // CAT
+        returnTotalDifference(Arrays.asList(10, 2, 3), Arrays.asList(1, 2, 3)); // returns 9
+//        returnTotalDifference(ArrayList<Integer>(1,2,3), ArrayList<Integer> (4,5,6) )
+//        returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7)); // returns 3
+//        returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7, 1)); // returns 2
     }
 }
+
+//In `codeup-java-exercises`, in `JavaDrills`:
+//        Create a static method, returnTotalDifference, that takes in two lists of integers and returns the difference between the sum of all integers in the first list minus the sum of all integers in the second list.
+//        returnTotalDifference(Arrays.asList(10, 2, 3), Arrays.asList(1, 2, 3)); // returns 9
+//        returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7)); // returns 3
+//        returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7, 1)); // returns 2

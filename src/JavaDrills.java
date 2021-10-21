2359e608a49df748f5c6217300f3b027ea08f884
Paris Shirley <shirleyp1097@gmail.com>
2021-10-21 09:21:02 -0500

    complete warmup

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+import java.util.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -21,30 +21,39 @@ public class JavaDrills {
 
     }
 
-    public static int returnTotalDifference(ArrayList<Integer> arr, ArrayList<Integer> arr2) {
-        int total = 0;
-        for (int i = 0; i < arr.size();  i++) {
-            total += arr.get(i);
-        }
-        for (int i = 0; i < arr2.size();  i++) {
-            total += arr2.get(i);
-        }
-        return total;
-    }
-
-    public static Dimension returnTotalDifference(List<Integer> arr, List arr2) {
-        int total = 0;
-        return arr.size();
+//    public static int returnTotalDifference(ArrayList<Integer> arr, ArrayList<Integer> arr2) {
+//        int total = 0;
 //        for (int i = 0; i < arr.size();  i++) {
 //            total += arr.get(i);
 //        }
+//        for (int i = 0; i < arr2.size();  i++) {
+//            total += arr2.get(i);
+//        }
+//        return total;
+//    }
+
+    public static String flipInnerCase(String word) {
+        String output = "";
+        for (int i = 0; i < word.length(); i++) {
+            if ((i == 0) || (i == word.length() - 1)) {
+                output += word.charAt(i);
+            } else {
+                if (Character.isUpperCase(word.charAt(i))) {
+                    output += Character.toLowerCase(word.charAt(i));
+                } else {
+                    output += Character.toUpperCase(word.charAt(i));
+                }
+            }
+        }
+        return output;
     }
 
     public static void main(String[] args) {
-        returnTotalDifference(Arrays.asList(10, 2, 3), Arrays.asList(1, 2, 3)); // returns 9
-//        returnTotalDifference(ArrayList<Integer>(1,2,3), ArrayList<Integer> (4,5,6) )
-//        returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7)); // returns 3
-//        returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7, 1)); // returns 2
+        System.out.println(flipInnerCase("cat")); // cAt
+        System.out.println(flipInnerCase("CaT")); // CAT
+        System.out.println(flipInnerCase("caT")); // cAT
+        System.out.println(flipInnerCase("codeup")); // cODEUp
+        System.out.println(flipInnerCase("CoDeup")); // COdEUp
     }
 }
 
